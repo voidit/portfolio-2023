cb1922b75b5998de348de8c5a9eee61036741009
Irena Romendik <kidnemor@gmail.com>
2023-10-11 19:29:30 -0400

    Updates

---
+++ _app/immutable/chunks/paths.7c07488b.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_426l4g)==null?void 0:s.base)??"/portfolio-2023";var t;const e=((t=globalThis.__sveltekit_426l4g)==null?void 0:t.assets)??a;export{e as a,a as b};
