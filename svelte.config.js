2d1b2c93383b28313a1b4439d4b7cca96e90188e
Irena Romendik <kidnemor@gmail.com>
2023-10-12 16:24:04 -0400

    removed adapter from svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -1,32 +1,13 @@
-import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
-	kit: {
-		adapter: adapter({
-			fallback: '400.html' // may differ from host to host
-		}),
-		paths: {
-			base: process.env.NODE_ENV === ('github-pages' || 'production') ? '/portfolio-2023/' : '',
-			assets: process.env.NODE_ENV === ('github-pages' || 'production') ? '/portfolio-2023/' : '',
-			// base: '' | `/portfolio-2023/` | `https://voidit.github.io/portfolio-2023/`,
-			// assets: '' | `/portfolio-2023/` | `https://voidit.github.io/portfolio-2023/`,
-			relative: true,
-		},
-		prerender: {
-			handleHttpError: ({ path, referrer, message }) => {
-				// ignore deliberate link to shiny 404 page
-				if (path === '/not-found' && referrer === '/400.html') {
-					return;
-				}
-
-				// otherwise fail the build
-				throw new Error(message);
-			}
-		}
-	}
+    kit: {
+        // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+        // If your environment is not supported or you settled on a specific environment, switch out the adapter.
+        // See https://kit.svelte.dev/docs/adapters for more information about adapters.
+        adapter: adapter()
+    }
 };
 
 export default config;
