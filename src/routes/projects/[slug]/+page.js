5aee1a4515cf74b799ab17ff96c83701951be646
Irena Romendik <kidnemor@gmail.com>
2023-10-13 15:11:34 -0400

    removed server

--- src/routes/projects/[slug]/+page.js
+++ src/routes/projects/[slug]/+page.js
@@ -1,9 +1,1 @@
-/** @type {import('./$types').PageLoad} */
-export function load({ params }) {
-    return {
-        project: {
-            title: `Title for ${params.slug} goes here`,
-            content: `Content for ${params.slug} goes here`
-        }
-    };
-}
+
--- src/routes/projects/[slug]/+page.server.js
+++
@@ -1,8 +0,0 @@
-import * as db from '$lib/server/database';
-
-/** @type {import('./$types').PageServerLoad} */
-export async function load({ params }) {
-    return {
-        project: await db.getProject(params.slug)
-    };
-}
